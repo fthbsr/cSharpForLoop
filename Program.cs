05a22f7004a681f80fbe0e10a3ed4d7d4971d77d
fatihbasar <fbasar680@gmail.com>
2022-03-11 16:05:54 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace for_loop
+{
+    class Program
+    {
+        static   void Main(string[] args)
+        {
+            int sayi = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < sayi; i++)
+            {
+                if( i % 2 == 1)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+        
+
+        int tekToplam = 0;
+        int ciftToplam = 0;
+
+         for (int i = 0; i < 1000; i++)
+            {
+                if( i % 2 == 1)
+                {
+                    tekToplam += i;
+                    
+                }else{
+                    ciftToplam += i;
+                }
+            }
+            Console.WriteLine("tek Toplam" + tekToplam);
+            Console.WriteLine("Cift Toplam" + ciftToplam);
+
+            for(int i = 0 ; i <= 10; i++ ){
+                
+                if( i == 4){
+                    break;
+                }
+                Console.WriteLine(i);
+                
+            }
+
+             for(int i = 0 ; i <= 10; i++ ){
+                
+                if( i == 4){
+                    continue;
+                }
+                 Console.WriteLine(i);
+
+                
+            }
+        }
+    }
+} 
